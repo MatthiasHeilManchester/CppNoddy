509469d3bf209f7f0f57b47fbb3b660b92228c62
Matthias Heil <M.Heil@maths.manchester.ac.uk>
2021-11-11 14:33:35 +0000

    reversed Rich's original file

--- Tests/EVP/EVPOrrSommerfeld_lapack.cpp
+++ Tests/EVP/EVPOrrSommerfeld_lapack.cpp
@@ -18,160 +18,21 @@
 using namespace CppNoddy;
 using namespace std;
 
-// hacky include; stolen from oomph-lib; no include guards and stuff. 
-#include "command_line_args.h"
-
-// Other includes
-#include <cfloat>    /* dbl_max */
-#include <cmath>     /* erf */
-  
-///////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////
-
-namespace Parameters
+int main()
 {
+  cout << "\n";
+  cout << "=== EVP: Temporal spectra of the Orr-Sommerfeld eqn =\n";
+  cout << "===  with a matrix problem assembled by hand.\n";
+  cout << "\n";
 
- /// Which base flow profile are we using
- enum {Poiseuille,Cylinder_wake};
-
- /// Enumerated base flow
- unsigned Base_flow=Cylinder_wake; //Poiseuille;
-
- // Base wavenumber (to fit into 2D box for Jesper)
- double Base_wave_number_alpha=1.0;
- 
- /// Number of waves in box
- unsigned N_waves_in_box=1;
- 
- // Reynolds number
- double Reynolds_number=10.0; // 5772.2;
- 
- /// Multiplier for exponential term in Cylinder wake profile
- // (max. velocity in free-stream (1.0) is depressed by that amount)
- double B_cylinder_wake=0.5;
-
- // Number of nodes in y direction
- unsigned Nnod_y=301;
- 
- // Number of nodes in x direction (used for output only)
- unsigned Nnod_x=200;
- 
- /// Height of lower boundary
- double Y_min=-5.0;
-
- /// Height of upper boundary
- double Y_max=5.0;
- 
- /// compute: base flow profile u(y), its second deriv. d^2u/dy^2, the
- /// associated stream function psi and the vorticity omega.
- void base_flow(const double& y,
-                double& u,
-                double& u_dash_dash,
-                double& psi,
-                double& omega)
- {
-  if (Base_flow==Poiseuille)
-   {
-    // Paranoia
-    if ((Y_min!=-1.0)||(Y_max!=1.0))
-     {
-      cout << "Wrong geometry for Poiseuille flow\n";
-      abort();
-     }
-    u = ( 1.0 - y * y );
-    u_dash_dash = -2.0;
-    psi=y-1.0/3.0*y*y*y+2.0/3.0; // constant chosen so that psi=0 at lower wall.
-    omega=-2.0*y;
-   }
-  else if (Base_flow==Cylinder_wake)
-   {
-    // Veloc non-dim on max. velocity (outside wake) and length on "width"
-    // of wake.
-    u = 1.0-B_cylinder_wake*exp(-y*y);
-    u_dash_dash = 2.0*B_cylinder_wake*exp(-y*y)*(1.0-2.0*y*y);
-    psi=y-B_cylinder_wake/2.0*sqrt(4.0*atan(1.0))*erf(y);
-    omega=-2.0*y*B_cylinder_wake*exp(-y*y);
-   }
-  else
-   {
-    cout << "Never get here. Base_flow = " << Base_flow << std::endl;
-    abort();
-   }
- }
-
-}
-
-
-
-///////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////
-
-
-
-int main(int argc, char* argv[])
-{
-
- // Store command line arguments
- CommandLineArgs::setup(argc,argv);
-
- // Number of nodes in y direction
- CommandLineArgs::specify_command_line_flag("--nnod_y",
-                                            &Parameters::Nnod_y);
-
- // Number of nodes in x directio (for output only)
- CommandLineArgs::specify_command_line_flag("--nnod_x",
-                                            &Parameters::Nnod_x);
-
- // Upper end of domain
- CommandLineArgs::specify_command_line_flag("--y_max",
-                                            &Parameters::Y_max);
- 
- // Lower end of domain
- CommandLineArgs::specify_command_line_flag("--y_min",
-                                            &Parameters::Y_min);
- 
- // Base wavenumber (to fit into box for Jesper)
- CommandLineArgs::specify_command_line_flag(
-  "--base_wave_number_alpha",
-  &Parameters::Base_wave_number_alpha);
-
- // Integer number of waves to fix into box
- CommandLineArgs::specify_command_line_flag("--n_waves_in_box",
-                                            &Parameters::N_waves_in_box);
-
- // Reynolds number
- CommandLineArgs::specify_command_line_flag("--reynolds_number",
-                                            &Parameters::Reynolds_number);
-
- // Parse command line
- CommandLineArgs::parse_and_assign(); 
- 
- // Doc what has actually been specified on the command line
- CommandLineArgs::doc_specified_flags();
-
- // Set actual parameters
- double wave_number_alpha=Parameters::Base_wave_number_alpha*
-  double(Parameters::N_waves_in_box);
- 
- cout << "Wavenumber: " << wave_number_alpha << std::endl;
- 
-
- //--------------------------------------
- // From here it's basically Rich's code
- //--------------------------------------
 
   // discretise with these many nodal points
- const std::size_t nodes( Parameters::Nnod_y);
-  
+  const std::size_t nodes( 301 );
   // we'll solve as TWO second order problems
   const std::size_t N( 2 * nodes );
-
   // domain boundaries
-  const double left  = Parameters::Y_min;
-  const double right = Parameters::Y_max;
-
+  const double left = -1.0;
+  const double right = 1.0;
   // spatial step for a uniform mesh
   const double d = ( right - left ) / ( nodes - 1 );
 
@@ -179,11 +40,9 @@ int main(int argc, char* argv[])
   DenseMatrix<D_complex> a( N, N, 0.0 );
   DenseMatrix<D_complex> b( N, N, 0.0 );
 
-
   // streamwise wavenumber and Reynolds number
-  const double alpha ( wave_number_alpha *
-                       double(Parameters::N_waves_in_box) );
-  const double Re ( Parameters::Reynolds_number ); 
+  const double alpha ( 1.02 );
+  const double Re ( 5772.2 );
   const D_complex I( 0.0, 1.0 );
 
   // boundary conditions at the left boundary
@@ -196,13 +55,9 @@ int main(int argc, char* argv[])
   {
     // position in the channel
     const double y = left + i * d;
-
-    // Base flow
-    double U=0.0;
-    double Udd=0.0;
-    double psi_base=0.0;
-    double omega_base=0.0;
-    Parameters::base_flow(y,U,Udd,psi_base,omega_base);
+    // Poiseuille flow profile
+    const double U = ( 1.0 - y * y );
+    const double Udd = -2.0;
 
     // the first quation at the i'th nodal point
     std::size_t row = 2 * i;
@@ -238,148 +93,27 @@ int main(int argc, char* argv[])
     cout << " \033[1;31;48m  * FAILED THROUGH EXCEPTION BEING RAISED \033[0m\n";
     return 1;
   }
-
-  // This was Rich's setup for Poiseuille flow
-  // // tag any eigenvalues with imaginary part > -0.1
-  //system.set_shift( D_complex( 0.0, -0.1 ) );
-  // system.tag_eigenvalues_upper( + 1 );
-
-  // Get all eigenvalues with an imaginary part less than 0.1
-  system.set_shift( D_complex( 0.0, 0.1 ) );
-  system.tag_eigenvalues_lower( + 1 );
+  // tag any eigenvalues with imaginary part > -0.1
+  system.set_shift( D_complex( 0.0, -0.1 ) );
+  system.tag_eigenvalues_upper( + 1 );
   lambdas = system.get_tagged_eigenvalues();
-
-  cout << "Getting eigenvectors\n";
-  DenseMatrix<D_complex> eig_vecs=system.get_tagged_eigenvectors();
-  cout << "Done: Got " << eig_vecs.nrows() << " eigenvectors of length "
-       << eig_vecs.ncols() << endl;
+  //lambdas.dump();
+  double min_growth_rate( lambdas[ 0 ].imag() );
+  // make sure we have a near neutral mode
+  const double tol = 1.e-3;
 
   std::string dirname("./DATA");
   mkdir( dirname.c_str(), S_IRWXU );
-
   TrackerFile spectrum( "./DATA/spectrum.dat" );
   spectrum.push_ptr( &lambdas, "evs" );
   spectrum.update();
- 
+  if ( std::abs( min_growth_rate ) < tol )
+  {
+    cout << "\033[1;32;48m  * PASSED \033[0m\n";
+    return 0;
+  }
 
- //--------------------------------------
- // End of Rich's code
- //--------------------------------------
-
-  
-  // Eigenvecs are rows of matrix -- how many do we have?
-  unsigned n_ev=eig_vecs.nrows();
-
-  // Do post-processing for the one with the smallest growth/decay rate
-  unsigned i_ev=0; 
-  double smallest_abs_imag=DBL_MAX;
-  for (unsigned i=0;i<n_ev;i++)
-   {
-    if (abs(lambdas[i].imag())<smallest_abs_imag)
-     {
-      smallest_abs_imag=abs(lambdas[i].imag());
-      i_ev=i;
-      // cout << "New ev with smallest absolute imag part: " << i_ev << " "
-      //      <<  smallest_abs_imag << std::endl;
-     }
-   }
-  complex<double> omega_freq=lambdas[i_ev]*wave_number_alpha;
-  cout << "Outputting fields for eigenvalue (c=omega/alpha): "
-       << lambdas[i_ev] << " corresponding to omega = "
-       << omega_freq << std::endl;
-
-  // Normalise on peturbation to streamfct 
-  double norm=0.0;
-  for (unsigned i=0;i<Parameters::Nnod_y;i++)
-   {
-    norm+=
-     pow(eig_vecs[i_ev][2*i].real(),2)+
-     pow(eig_vecs[i_ev][2*i].imag(),2);
-   }
-  norm=sqrt(norm/double(Parameters::Nnod_y));
-  for (unsigned i=0;i<2*Parameters::Nnod_y;i++)
-   {
-    eig_vecs[i_ev][i]/=norm;
-   }
-
-
-  // Output
-  ofstream outfile;
-  std::string filename=dirname+"/one_d_field.dat";
-  outfile.open(filename.c_str());
-  outfile << "# Eigenvalue (c=omega/alpha): " << lambdas[i_ev]
-          << " ; omega = " << omega_freq << std::endl;
-  for (unsigned i=0;i<Parameters::Nnod_y;i++)
-   {
-    const double y = left + double(i) * d;
-
-    // Base flow
-    double U=0.0;
-    double Udd=0.0;
-    double psi_base=0.0;
-    double omega_base=0.0;
-    Parameters::base_flow(y,U,Udd,psi_base,omega_base);
-
-
-    // We're plotting quantities across width of domain:  y, phi(y) (complex),
-    // vort(y) (complex), u_base, u_base'', phi_base, vort_base 
-    outfile << y << " "
-            << eig_vecs[i_ev][2*i].real() << " "
-            << eig_vecs[i_ev][2*i].imag() << " "
-            << -eig_vecs[i_ev][2*i+1].real() << " "
-            << -eig_vecs[i_ev][2*i+1].imag() << " "
-            << U << " "
-            << Udd << " "
-            << psi_base << " "
-            << omega_base << " " 
-            << std::endl;
-   }
-  outfile.close();
-  
-  
-  // Output 2D streamfunction and vorticity at grid points (as initial
-  // condition for Jesper)
-
-  // Randomly do it at t=0
-  double time=0.0;
-  filename=dirname+"/two_d_field.dat";
-  outfile.open(filename.c_str());
-  outfile << "ZONE I=" << Parameters::Nnod_x << ", J=" << Parameters::Nnod_y 
-          << std::endl; 
-  for (unsigned i=0;i<Parameters::Nnod_y;i++)
-   {
-    const double y = left + double(i) * d;
-    complex<double> phi=eig_vecs[i_ev][2*i];
-    complex<double> psi=eig_vecs[i_ev][2*i+1];
-    
-    // Base flow
-    double U=0.0;
-    double Udd=0.0;
-    double psi_base=0.0;
-    double omega_base=0.0;
-    Parameters::base_flow(y,U,Udd,psi_base,omega_base);
-    Parameters::base_flow(y,U,Udd,psi_base,omega_base);
-    
-    for (unsigned j=0;j<Parameters::Nnod_x;j++)
-     {
-      const double x = double(j)/double(Parameters::Nnod_x-1)*
-       (2.0*4.0*atan(1.0))/Parameters::Base_wave_number_alpha;
-      
-      // We're outputting: x, y, Phi_pert, Vort_pert, Phi_base, Vort_base
-      outfile << x << " "
-              << y << " "
-              <<  (phi*exp(I*(wave_number_alpha*x-omega_freq*time))).real()
-              << " " 
-              << -(psi*exp(I*(wave_number_alpha*x-omega_freq*time))).real()
-              << " "
-              << psi_base << " "
-              << omega_base << " " 
-              << std::endl;
-     }
-   }
-  outfile.close();
-
-  
-  return 0;
-  
+  cout << "\033[1;31;48m  * FAILED \033[0m\n";
+  cout << "    Final error = " << min_growth_rate << "\n";
+  return 1;
 }
